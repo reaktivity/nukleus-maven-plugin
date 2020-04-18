bd8d5c8edd3040b46dcb3b3956df7bc1a8ab596d
Izzy Moriguchi <izumimoriguchi@gmail.com>
2020-04-17 17:46:45 -0700

    Delete manually designed classes and update tests

--- src/test/java/org/reaktivity/nukleus/maven/plugin/internal/generated/UnionWithEnumFWTest.java
+++ src/test/java/org/reaktivity/nukleus/maven/plugin/internal/generated/UnionWithEnumFWTest.java
@@ -26,6 +26,7 @@ import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Test;
 import org.reaktivity.reaktor.internal.test.types.inner.EnumWithInt8;
+import org.reaktivity.reaktor.internal.test.types.inner.EnumWithUint8;
 import org.reaktivity.reaktor.internal.test.types.inner.EnumWithVariantOfUint64;
 import org.reaktivity.reaktor.internal.test.types.inner.ListWithEnumAndVariantWithDefaultFW;
 import org.reaktivity.reaktor.internal.test.types.inner.UnionWithEnumFW;
